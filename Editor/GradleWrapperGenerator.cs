b064ea39ae407b5173486074099747cab011c7d8
gilzoide <gilzoide@gmail.com>
2022-06-28 19:56:50 -0300

    Use configured gradlePath if possible, fix PlaybackEngines path on macOS

--- Editor/GradleWrapperGenerator.cs
+++ Editor/GradleWrapperGenerator.cs
@@ -9,18 +9,6 @@ namespace Gilzoide.GradleWrapperGenerator.Editor
 {
     public class GradleWrapperGenerator : IPostGenerateGradleAndroidProject
     {
-        static readonly string EXE_EXTENSION = Path.GetExtension(EditorApplication.applicationPath);
-        static readonly string UNITY_ANDROID_PATH = Path.Combine(
-            EditorApplication.applicationContentsPath, "PlaybackEngines", "AndroidPlayer"
-        );
-        static readonly string UNITY_JAVA_EXE = Path.Combine(
-            UNITY_ANDROID_PATH, "OpenJDK", "bin", "java" + EXE_EXTENSION
-        );
-        static readonly string GRADLE_JAR = FindFirstFileWithPattern(
-            Path.Combine(UNITY_ANDROID_PATH, "Tools", "gradle", "lib"),
-            "gradle-launcher*.jar"
-        );
-
         public int callbackOrder => 0;
 
         public void OnPostGenerateGradleAndroidProject(string path)
@@ -49,8 +37,8 @@ namespace Gilzoide.GradleWrapperGenerator.Editor
 
             var startInfo = new ProcessStartInfo()
             {
-                FileName = GetJavaExecutable(),
-                Arguments = $"-jar \"{GRADLE_JAR}\" wrapper --gradle-version {gradleVersion} -b {emptyGradleScriptFile}",
+                FileName = FindJavaExecutable(),
+                Arguments = $"-jar \"{FindGradleJar()}\" wrapper --gradle-version {gradleVersion} -b {emptyGradleScriptFile}",
                 RedirectStandardError = true,
                 UseShellExecute = false,
                 WindowStyle = ProcessWindowStyle.Hidden,
@@ -71,27 +59,45 @@ namespace Gilzoide.GradleWrapperGenerator.Editor
             File.Delete(emptyGradleScriptPath);
         }
 
-        public static string GetJavaExecutable()
+        public static string FindJavaExecutable()
         {
 #if UNITY_2019_3_OR_NEWER
-            if (!string.IsNullOrEmpty(AndroidExternalToolsSettings.jdkRootPath))
-            {
-                string javaExecutable = Path.Combine(AndroidExternalToolsSettings.jdkRootPath, "bin", "java" + EXE_EXTENSION);
-                if (File.Exists(javaExecutable))
-                {
-                    return javaExecutable;
-                }
-
-                Debug.LogWarning($"[{nameof(GradleWrapperGenerator)}] Unable to find Java executable in External Tools JDK path '{AndroidExternalToolsSettings.jdkRootPath}'. Searched for executable file '{javaExecutable}'");
-            }
+            string javaRoot = !string.IsNullOrEmpty(AndroidExternalToolsSettings.jdkRootPath)
+                ? AndroidExternalToolsSettings.jdkRootPath
+                : Path.Combine(GetUnityAndroidPlayerRoot(), "OpenJDK");
+#else
+            string javaRoot = Path.Combine(GetUnityAndroidPlayerRoot(), "OpenJDK");
 #endif
-            if (File.Exists(UNITY_JAVA_EXE))
+            string javaExe = Path.Combine(javaRoot, "bin", "java" + Path.GetExtension(EditorApplication.applicationPath));
+            if (File.Exists(javaExe))
             {
-                return UNITY_JAVA_EXE;
+                return javaExe;
             }
 
-            Debug.LogWarning($"[{nameof(GradleWrapperGenerator)}] Unable to find Java executable installed with Unity editor. Falling back to system's `java` command");
+            Debug.LogWarning($"[{nameof(GradleWrapperGenerator)}] Unable to find Java executable at '{javaExe}'. Falling back to system's `java` command");
             return "java";
+        }
+
+        public static string FindGradleJar()
+        {
+#if UNITY_2019_3_OR_NEWER
+            string gradleRoot = !string.IsNullOrEmpty(AndroidExternalToolsSettings.gradlePath)
+                ? AndroidExternalToolsSettings.gradlePath
+                : Path.Combine(GetUnityAndroidPlayerRoot(), "Tools", "gradle");
+#else
+            string gradleRoot = Path.Combine(GetUnityAndroidPlayerRoot(), "Tools", "gradle");
+#endif
+            return FindFirstFileWithPattern(Path.Combine(gradleRoot, "lib"), "gradle-launcher*.jar");
+        }
+
+        public static string GetUnityAndroidPlayerRoot()
+        {
+            string unityRoot = Path.GetDirectoryName(EditorApplication.applicationPath);
+#if UNITY_EDITOR_OSX
+            return Path.Combine(unityRoot, "PlaybackEngines", "AndroidPlayer");
+#else
+            return Path.Combine(unityRoot, "Data", "PlaybackEngines", "AndroidPlayer");
+#endif
         }
 
         public static string FindFirstFileWithPattern(string dir, string pattern)
